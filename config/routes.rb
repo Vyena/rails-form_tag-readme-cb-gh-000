abb6ead1d4638175e82cfb08fb7faf818881f58c
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-19 22:38:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  # resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+  # when a POST request is sent to the /posts resource, route to the create method in the posts controller
 end
